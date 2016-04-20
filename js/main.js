746e03a0c162ba12e6e4479fc2ad8f60b0765e8f
Chloe Weber <chloeesierraa@yahoo.com>
2016-04-19 21:28:31 -0500

    Add beginning portion of Javascript code

--- js/main.js
+++ js/main.js
@@ -10,3 +10,18 @@ $( document ).ready( function() {
 $( document ).ready( function() {
     $( '.drawer' ).drawer();
 } );
+
+var imgOne = document.getElementById( 'img-one' );
+imgOne.addEventListener( 'click', mouseOver );
+imgOne.addEventListener( 'mouseout', mouseOut );
+
+function mouseOver() {
+    console.log( 'yay' );
+    imgOne.style.width = ' 64px ';
+    imgOne.style.height = ' 64px ';
+}
+
+function mouseOut() {
+    imgOne.style.width = ' 50px ';
+    imgOne.style.height = ' 50px ';
+}
